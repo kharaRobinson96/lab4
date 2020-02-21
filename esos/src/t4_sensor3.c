b9c7c0672be7f1d03fc825f22b644489af6b1150
kharaRobinson96 <43385947+kharaRobinson96@users.noreply.github.com>
2020-02-21 11:44:13 -0600

    Create t4_sensor3.c

---
+++ esos/src/t4_sensor3.c
@@ -0,0 +1,3 @@
+// Create an ESOS application called t4_sensor3 that is a modified version of your application t4_sensor2 , 
+// but displays the ambient board temperature from the LM60. 
+// Your application should still allow averaging, minimums, maximums, and median filtering.

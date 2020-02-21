dc53a56cf9ee867fd3f9dd6384c480774c48190c
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-02-20 18:58:40 -0600

    Further scons fixes

--- SCons_esos.py
+++ SCons_esos.py
@@ -53,8 +53,8 @@ for sourceFile in Glob('BUILD_DIR/*.c', True, True, True):
         'esos/src/pic24/esos_pic24_spi.c',
         'esos/src/pic24/esos_pic24_tick.c', 
         'esos/src/esos_f14ui.c',
-        'src/pic24/esos_pic24_sensor.c',
-        'lib/src/esos_sensor.c'])
+        'esos/src/pic24/esos_pic24_sensor.c',
+        'esos/src/esos_sensor.c'])
     linker_side_effect(env, p)
     # Convert it to a .hex
     bin2hex(sourceFile, env, 'esos')
